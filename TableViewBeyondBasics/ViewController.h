e5c0304b466c438ba68e210f7af9eac4528994a3
Ali Kooshesh <blkberryjava@gmail.com>
2014-03-06 09:59:43 -0800

    Initial Commit

---
+++ TableViewBeyondBasics/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableViewBeyondBasics
+//
+//  Created by AAK on 3/6/14.
+//  Copyright (c) 2014 Ali Kooshesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TableViewBeyondBasicsTests/TableViewBeyondBasicsTests.m
@@ -0,0 +1,34 @@
+//
+//  TableViewBeyondBasicsTests.m
+//  TableViewBeyondBasicsTests
+//
+//  Created by AAK on 3/6/14.
+//  Copyright (c) 2014 Ali Kooshesh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewBeyondBasicsTests : XCTestCase
+
+@end
+
+@implementation TableViewBeyondBasicsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
